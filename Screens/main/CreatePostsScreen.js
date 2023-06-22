fbc715a6f9bead90d1323ed6afc6dcf8fc5a3580
Taisiya Tomashevska <taya22011985@gmail.com>
2023-06-22 22:33:55 +0300

    CREATE: fn takePhoto

--- Screens/main/CreatePostsScreen.js
+++ Screens/main/CreatePostsScreen.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState} from "react";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import { Camera } from "expo-camera";
 import { IconButton } from "@react-native-material/core";
 import { MaterialIcons } from '@expo/vector-icons';
 
+
+
 export const
     CreatePostsScreen = () => { 
+        const [camera, setCamera] = useState(null);
+        const [photo, setPhoto] = useState(null);
+
+        const takePhoto = async () => { 
+            const photo = await camera.takePictureAsync();
+            setPhoto(photo.uri);
+            console.log(photo);
+        }
+
         return (<View style={styles.container}>
-            <Camera style={styles.camera}>
-                <IconButton style={styles.iconBorder} onPress={() => { }}
+            <Camera style={styles.camera} ref={setCamera}> 
+                {photo && <View style={styles.photoContainer}>
+                    <Image source={{ uri: photo }} style={{width: 180, height:220} } />
+                </View>}
+                
+                <IconButton style={styles.iconBorder} onPress={takePhoto}
               icon={(props) => (
-                        <MaterialIcons
-                            
+                        <MaterialIcons          
                   name="camera-alt"
                   {...props}
                   color="#BDBDBD"
@@ -29,11 +43,14 @@ styles = StyleSheet.create({
       
     },
     camera: {
-        height: 240,
+        // height: 600,
+      
+        flex: 1,
         alignItems: "center",
         justifyContent: "center",
         marginHorizontal: 16,
         marginTop: 32,
+      
     },
     iconBorder: {
         borderRadius: 50, 
@@ -41,6 +58,12 @@ styles = StyleSheet.create({
         height: 60,
         backgroundColor:"#FFF"
        
-
+    },
+    photoContainer: {
+        position: "absolute",
+        top: 30,
+        left:30,
+        borderWidth: 1,
+        borderColor:"#FFF"
     }
 })
