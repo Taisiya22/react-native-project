468c815259df3e92eddb0649947e7b2042d52dcb
Taisiya Tomashevska <taya22011985@gmail.com>
2023-06-22 20:14:15 +0300

    ADDED: camera to createScreen

--- Screens/main/CreatePostsScreen.js
+++ Screens/main/CreatePostsScreen.js
@@ -1,15 +1,47 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import { IconButton } from "@react-native-material/core";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export const
     CreatePostsScreen = () => { 
-    return(<View style={ styles.container}><Text>CreatePostsScreen</Text></View>)
+        return (<View style={styles.container}>
+            <Camera style={styles.camera}>
+  <IconButton style={styles.iconBorder}
+              icon={(props) => (
+                        <MaterialIcons
+                            
+                  name="camera-alt"
+                  {...props}
+                  color="#BDBDBD"
+                  size={24}
+                />
+              )}
+            />
+                </Camera>
+    </View>)
     }
 
 styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems:'center'
+      
+     
+    },
+    camera: {
+        height: 240,
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 16,
+        marginTop: 32,
+    },
+    iconBorder: {
+        borderRadius: 50, 
+        width: 60,
+        height: 60,
+        backgroundColor:"#FFF"
+       
+
     }
 })
